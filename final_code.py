16ccc85d9c92cd1a96c6df78cdf51f936e2efd81
pranoy-panda <pranoy-panda@users.noreply.github.com>
2017-11-07 20:26:42 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -8,7 +8,7 @@ from dijstra import *
 import numpy as np
 import cv2
 import math
-
+from trackbar_setting import *
 '''
 global variables
 '''
@@ -16,28 +16,6 @@ refPt = []
 cropping = False
 inf = float("9e999")
  
-def click_and_crop(event, x, y, flags, param):
-	# grab references to the global variables
-	global refPt, cropping
- 
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being performed
-
-	if event == cv2.EVENT_LBUTTONDOWN:
-		refPt = [(x, y)] # reference point for cropping
-		cropping = True
- 
-	# check to see if the left mouse button was released
-	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
-		refPt.append((x, y))
-		cropping = False
- 
-		# drawing a rectangle around the region of interest
-		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-		cv2.imshow("image", image)
-
 def find_shape(c):
     shape = "unidentified"
     peri = cv2.arcLength(c, True)
@@ -62,53 +40,7 @@ def angle(pt1,pt2): # pt1 is the source and pt2 is destination pt1-->pt2
 The below give function takes input as a image and returns a path from source(brown) to destination(green)
 by visiting all yellow objects in the image
 '''
-def part1(image): 
-	clone = image.copy()
-	cv2.namedWindow("image")
-	cv2.setMouseCallback("image", click_and_crop)
-	list_of_min_vals= []
-	list_of_max_vals =[] 
-	num_of_unique_colors = 4 #(white(obstacles), bot(brown), objects(yellow), end(green))
-	# keep looping until the 'q' key is pressed
-	for i in xrange(num_of_unique_colors):	
-		clone = image.copy()
-		cv2.namedWindow("image")
-		cv2.setMouseCallback("image", click_and_crop)
-		 
-		# keep looping until the 'q' key is pressed
-		while True:
-			# display the image and wait for a keypress
-			cv2.imshow("image", image)
-			key = cv2.waitKey(1) & 0xFF
-		 
-			# if the 'r' key is pressed, reset the cropping region
-			if key == ord("r"):
-				image = clone.copy()
-		 
-			# if the 'c' key is pressed, break from the loop
-			elif key == ord("c"):
-				break
-		 
-		# if there are two reference points, then crop the region of interest
-		# from the image and display it
-		if len(refPt) == 2:
-			roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-			max_val_R = roi[:,:,2].max() # max val of red channel in roi
-			max_val_G = roi[:,:,1].max() # max val of green channel in roi
-			max_val_B = roi[:,:,0].max() # max val of blue channel in roi
-
-			min_val_R = roi[:,:,2].min() # min val of red channel in roi
-			min_val_G = roi[:,:,1].min() # min val of green channel in roi
-			min_val_B = roi[:,:,0].min() # min val of blue channel in roi
-			print "max vals"
-			print str(max_val_R)+","+str(max_val_G)+","+str(max_val_B)
-			print "min vals"
-			print str(min_val_R)+","+str(min_val_G)+","+str(min_val_B)
-			cv2.imshow("roi", roi)
-			cv2.waitKey(0)
-		list_of_min_vals.append([min_val_B-10,min_val_G-10,min_val_R-10]) # [yellow,pink]	
-		list_of_max_vals.append([max_val_B+10,max_val_G+10,max_val_R+10]) # [yellow,pink]
-		cv2.destroyAllWindows()
+def part1(image,list_of_min_vals,list_of_max_vals): 
 	
 	'''
 	 initializing variables
@@ -126,22 +58,25 @@ def part1(image):
 	mask=cv2.inRange(image,lower_brown,upper_brown)
 	kernel = np.ones((5,5),np.uint8)
 	mask = cv2.dilate(mask,kernel,iterations = 1)
+	cv2.imshow('brown',mask)
+
 	im2,contours_brw,hierarchy = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 	for i in xrange(len(contours_brw)):
 		x,y,w,h = cv2.boundingRect(contours_brw[i])
-		nodes.append((x+w/2,y+h/2))
+		if w*h>1000:
+			nodes.append((x+w/2,y+h/2))
 	#########################################
 
 	'''
-	obstacles ---> to be avoided
+	#obstacles ---> to be avoided
 	'''
 	lower_white=np.array(list_of_min_vals[1])
 	upper_white=np.array(list_of_max_vals[1])
 
 	mask=cv2.inRange(image,lower_white,upper_white)
 	kernel = np.ones((5,5),np.uint8)
-	mask = cv2.dilate(mask,kernel,iterations = 12)
+	mask = cv2.dilate(mask,kernel,iterations = 15)
 	cv2.imshow('obs',mask)
 	im2,contours_ob,hierarchy = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
 	for i in xrange(len(contours_ob)):
@@ -188,7 +123,7 @@ def part1(image):
 	#########################################	
 
 	'''
-	 yellow object  ---> objects to be visited
+	 #yellow object  ---> objects to be visited
 	''' 
 	list_of_squares = []
 	list_of_triangles = []
@@ -198,6 +133,7 @@ def part1(image):
 	mask=cv2.inRange(image,lower_yellow,upper_yellow)
 	kernel = np.ones((5,5),np.uint8)
 	mask = cv2.dilate(mask,kernel,iterations = 1)
+	cv2.imshow('yellow',mask)
 	im2,contours_yel,hierarchy = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	# appending the nodes 
 	for i in xrange(len(contours_yel)):
@@ -210,11 +146,12 @@ def part1(image):
 		else:
 			list_of_triangles.append((cX,cY))	
 		x,y,w,h = cv2.boundingRect(contours_yel[i])
-		nodes.append((cX,cY))
+		if w*h>1000:	
+			nodes.append((cX,cY))
 	#########################################
 
 	'''
-	green object  ---> final destination
+	#green object  ---> final destination
 	'''
 	lower_green=np.array(list_of_min_vals[3])
 	upper_green=np.array(list_of_max_vals[3])
@@ -222,11 +159,13 @@ def part1(image):
 	mask=cv2.inRange(image,lower_green,upper_green)
 	kernel = np.ones((5,5),np.uint8)
 	mask = cv2.dilate(mask,kernel,iterations = 1)
+	cv2.imshow('green',mask)
 	im2,contours_yel,hierarchy = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	# appending the nodes 
 	for i in xrange(len(contours_yel)):
 		x,y,w,h = cv2.boundingRect(contours_yel[i])
-		nodes.append((x+w/2,y+h/2))
+		if w*h>1000:
+			nodes.append((x+w/2,y+h/2))
 	#########################################
 
 	list_of_neighbours = [] # stores the list of neighbours(where the first node in each neighbour is the primary node or should be the key of the graph)
@@ -240,8 +179,8 @@ def part1(image):
 		graph[neighbour[0]].extend(temp) 
 	
 	'''
-	the below written code finds a path between the start and end node via all other objects using nearest neighbour
-	paradigm
+	#the below written code finds a path between the start and end node via all other objects using nearest neighbour
+	#paradigm
 	'''
 	flip_flag = 1 # for alternate picks, if flag = 0, we need to pick square else we need to pick triangle
 	alternate_flag = 0
@@ -282,7 +221,7 @@ def part1(image):
 			for neighbour in list_of_neighbours:
 				graph[neighbour[0]] = neighbour[1:]
 				graph[neighbour[0]].extend(temp)		
-	print list_of_triangles == list_of_squares
+	#print list_of_triangles == list_of_squares
 	if alternate_flag == 1: # we need to visit alternate shapes while traversing from start to end node
 		for i in xrange(10):
 			if count == 0:
@@ -327,7 +266,7 @@ def part1(image):
 
 
 	'''
-	the following code draws the visibility graph on the image(visualization)
+	#the following code draws the visibility graph on the image(visualization)
 	'''	
 	for neighbour in list_of_neighbours:
 		if len(neighbour)==0:
@@ -338,7 +277,15 @@ def part1(image):
 			cv2.line(image,first_point,second_point,(0,0,255),2)
 	
 	visit = []	
-	print final_path		
+	print final_path
+	temp = final_path[0]
+
+	for i in xrange(len(final_path)-1):
+		if i+1==len(final_path):
+			break
+		if length_of_edge(final_path[i+1],temp)<50:
+			final_path.remove(final_path[i])	
+		temp = final_path[i+1]			
 	counter = 0
 	for i in xrange(len(final_path)):
 		cv2.putText(image,str(counter),final_path[i], cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)	
@@ -347,7 +294,9 @@ def part1(image):
 	cv2.imshow('image',image)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
-	return final_path
+
+	return final_path,list_of_squares,list_of_triangles
+
 
 def main(final_path):
 	global image
@@ -407,7 +356,7 @@ def main(final_path):
 	for pt in final_path[1:]:
 		while(length_of_edge(pt,center)>1):
 			ret,frame=cap.read()
-			frame=cv2.GaussianBlur(frame, (5,5),0)
+			#frame=cv2.GaussianBlur(frame, (5,5),0)
 
 			'''
 			pink marker
@@ -502,10 +451,12 @@ def main(final_path):
 	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	image = cv2.imread('dummy_image2.jpg')
-	image = cv2.resize(image,(649, 647))
+	global image
+	image = cv2.imread('Capture.JPG')
+	image = cv2.resize(image,(640, 480))
 	print 'crop out the start marker, white obstacle, yellow objects and end marker respectively'
-	final_path = part1(image)
-	#print 'crop out the head marker and tail marker of the bot respectively'
-	#main(final_path)	
-	#print 'work done'	
+	list_of_max_vals,list_of_min_vals = colored_object_tracker(image)
+	#print list_of_max_vals,list_of_min_vals
+	image = cv2.imread('Capture.JPG')
+	final_path,list_of_squares,list_of_triangles = part1(image,list_of_max_vals,list_of_min_vals)
+	print final_path
